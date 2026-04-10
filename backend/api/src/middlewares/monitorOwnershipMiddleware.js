5c83ee33f9d1ed9b45a7852c9dab8595cc6b39c2
Vishwas Kumar <vishwashverma1234@gmail.com>
2026-04-10 16:24:08 +0530

    fix(middlware): resolve minor bug in monitor ownership middleware

--- backend/api/src/middlewares/monitorOwnershipMiddleware.js
+++ backend/api/src/middlewares/monitorOwnershipMiddleware.js
@@ -1,12 +1,12 @@
-import Monitor from "../models/monitors.model.JS";
 import { getOneMonitorFromDB } from "../repositories/monitors.repository.js";
 import AppError from "../utils/appError.js";
 
 const verifyMonitorOwnership = async (req, res, next) => {
-  const monitor = await getOneMonitorFromDB(req.user,req.monitor);
+  console.log(req.params.id)
+  const monitor = await getOneMonitorFromDB(req.user,req.params.id);
   
   if(!monitor) return next(new AppError(403, "Forbidden"));
-  req.monitor = monitor;
+  req.monitor = monitor._id;
   next();
 }
 
