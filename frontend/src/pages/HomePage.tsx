8fee7bea4997589a2dd2065d3889f5d6fb6f4963
Vishwas Kumar <vishwashverma1234@gmail.com>
2026-04-17 12:55:14 +0530

    feat(ui): add home page

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,34 @@
+import {MonitorCheck, Activity, AlertOctagon, BarChart3} from "lucide-react"
+import { Link } from "react-router-dom"
+
+export default function Home() {
+    return (
+        <section className="section container-main">
+            <div className="section-tight text-hero flex flex-col gap-2 md:gap-3 ">
+                <h2>Monitor your APIs and websites with real-time uptime tracking</h2>
+                <p className="text-body">Get alerts when your services go down</p>
+                <div className="flex gap-main">
+                    <Link to={"/register"} className="btn-primary">Get Started</Link>
+                    <Link to={"/login"} className="btn-secondary">Login</Link>
+                </div>
+            </div>
+
+            <div className="column-flex mb-10">
+                <h3 className="text-title">Features</h3>
+                <div className="grid grid-cols-2 lg:grid-cols-4 gap-main">
+                    <div className="card card-title hover:-translate-y-0.5 flex flex-center gap-6"><MonitorCheck/> <span>Uptime Monitoring</span></div>
+                    <div className="card card-title hover:-translate-y-0.5 flex flex-center gap-6 "><Activity/> <span>Real-Time Status</span></div>
+                    <div className="card card-title hover:-translate-y-0.5 flex flex-center gap-6"><AlertOctagon/> <span>Incident Tracking</span></div>
+                    <div className="card card-title hover:-translate-y-0.5 flex flex-center gap-6"><BarChart3/> <span>Performance Insights</span></div>
+                </div>
+            </div>
+
+            
+            <div className="body-text column-flex ">
+                <p className="heading">Built with queue-based architecture (Redis + Workers) for scalable monitoring.</p>
+                <p>Start monitoring in seconds</p>
+                <Link to={"/register"} className="btn-primary w-fit">Get Started</Link>
+            </div>
+        </section>
+    )
+}
