0e2216775373a06b192c3df621230a7b612f841e
Vishwas Kumar <vishwashverma1234@gmail.com>
2026-04-26 11:14:50 +0530

    feat(test): add integration test for Auth login

---
+++ backend/api/tests/integration/auth.login.test.js
@@ -0,0 +1,70 @@
+import request from "supertest";
+import app from "../../../app.js";
+import User from "../../src/models/user.model.js";
+import {vi,beforeAll,beforeEach,afterAll, describe,it,expect} from "vitest";
+import { connectTestDB, clearTestDB,closeTestDB } from "../setup.js";
+
+describe("POST /api/auth/login (Integration)", ()=> {
+    beforeAll(connectTestDB);
+    afterAll(closeTestDB);
+    beforeEach(clearTestDB);
+
+    const registerUserData = (overrides={}) => ({
+      email: "test@example.com",
+      username: "testuser",
+      password: "StrongPass123!",
+      ...overrides
+    })
+
+    const loginUserData = (overrides={}) => ({
+      email: "test@example.com",
+      password: "StrongPass123!",
+      ...overrides
+    })
+
+    it("should login a user successfully", async()=> {
+        await request(app)
+        .post("/api/auth/register")
+        .send(registerUserData());
+
+        const loginPayload = loginUserData();
+
+        const response = await request(app)
+                            .post("/api/auth/login")
+                            .send(loginPayload);
+
+        const cookies = response.headers["set-cookie"]
+        expect(cookies).toBeDefined();
+
+        const accessCookie = cookies.find(c=> c.startsWith("accessToken="));
+
+        expect(accessCookie).toBeDefined();
+
+        expect(accessCookie).toContain("HttpOnly");
+
+
+        expect(response.status).toBe(200);
+        expect(response.body.message).not.toBeNull();
+        expect(response.body.data.user.password).toBeUndefined();
+    })
+
+    it("should return 401 when password is wrong", async()=> {
+    await request(app)
+        .post("/api/auth/register")
+        .send(registerUserData())
+
+    const loginPayload = loginUserData({password:"12345678"})
+
+    const response = await request(app)
+                        .post("/api/auth/login")
+                        .send(loginPayload)
+
+    const cookies = response.headers["set-cookie"]
+    expect(cookies).not.toBeDefined()
+
+    expect(response.status).toBe(401)
+    expect(response.body.message).not.toBeNull()
+    expect(response.body.data).toBeUndefined()
+    
+    })
+} )
