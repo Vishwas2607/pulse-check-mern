0ad6ab96315429947f5ddf9098346934e3ee81f6
Vishwas Kumar <vishwashverma1234@gmail.com>
2026-04-27 11:56:36 +0530

    feat(test): add unit test for worker aggregation service

---
+++ backend/worker/tests/unit/aggregation.test.js
@@ -0,0 +1,40 @@
+import {describe,it,expect,vi,beforeEach} from "vitest"; 
+
+vi.mock("../../../api/src/repositories/hourlyAggregate.repository.js", ()=> ({
+    upsertHourlyAggregation: vi.fn()
+}))
+
+vi.mock("../../src/utils/helpers.js", ()=> ({
+    floorToHour: vi.fn()
+}))
+
+import { updateAggregation } from "../../src/services/aggregation.service.js";
+import { upsertHourlyAggregation } from "../../../api/src/repositories/hourlyAggregate.repository.js";
+import { floorToHour } from "../../src/utils/helpers.js";
+
+describe("Aggregation Service - Update Aggregation", ()=> {
+    beforeEach(()=> {
+        vi.clearAllMocks();
+    });
+
+    const mockArguments = (overrides={}) => ({monitorId:"123", status:"up", latency: 256, created:true, ...overrides});
+    
+    it("should upsert aggregation data successfully when created is true", async()=> {
+        floorToHour.mockReturnValue("12/04/26, 11:00");
+        upsertHourlyAggregation.mockResolvedValue();
+
+        const result = await updateAggregation(mockArguments());
+
+        expect(result).toBeUndefined();
+        expect(upsertHourlyAggregation).toHaveBeenCalled()
+    })
+
+    it("should return without updating aggregation data when created is false", async()=> {
+
+        const result = await updateAggregation(mockArguments({created:false}));
+
+        expect(result).toBeUndefined();
+        expect(upsertHourlyAggregation).not.toHaveBeenCalled()
+    })
+
+});
