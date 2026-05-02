4c326137e2b3f6c34395f2040b4bc0c96ffb4b74
Vishwas Kumar <vishwashverma1234@gmail.com>
2026-05-02 09:12:07 +0530

    feat: add cron logic to bypass paid background task limits

--- backend/worker/src/index.js
+++ backend/worker/src/index.js
@@ -3,14 +3,29 @@ import ConnectDB from "../../shared/config/db.connection.js";
 import path from "path";
 import logger from "./utils/logger.js";
 import { startWorker } from "./worker.connection.js";
+import express from "express";
 
 dotenv.config({ path: path.resolve(process.cwd(), "../../.env") });
 
+const app = express();
+
+app.post("/run-cron", async(req,res) => {
+    const token = req.headers["x-cron-secret"];
+    if(token !== process.env.CRON_SECRET){
+        return res.status(403).send("Forbidden");
+    }
+    try {
+        await startWorker();
+        res.send("Worker executed");
+    } catch (err) {
+        res.status(500).send("Error Running worker")
+    }
+})
 if(process.env.NODE_ENV !== "test") {
     ConnectDB()
     .then(()=> {
         logger.info("✅ DB connected successfully");
-        startWorker();
+       app.listen(10000, ()=> console.log("Worker server running..."))
     }).catch((err)=> {
         logger.error({err},"❌ Failed to connect to DB")
         process.exit(1);
